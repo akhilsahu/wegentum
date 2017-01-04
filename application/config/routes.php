de6c708608424c54f0d2f8a54c47ff7d7f793e71
thirdthought1990 <thethirdthought1990@gmail.com>
2017-01-04 17:41:22 +0530

    themechange

--- application/config/routes.php
+++ application/config/routes.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | There are three reserved routes:
 |
-|	$route['default_controller'] = 'welcome';
+   $route['default_controller'] = 'welcome';
 |
 | This route indicates which controller class should be loaded if the
 | URI contains no data. In the above example, the "welcome" class
@@ -49,14 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['templates/(:any)'] = "templates/view/$1";
 
-$route['items'] = "items/index";
-$route['itemsCreate']['post'] = "items/store";
-$route['itemsEdit/(:any)'] = "items/edit/$1";
-$route['itemsUpdate/(:any)']['put'] = "items/update/$1";
-$route['itemsDelete/(:any)']['delete'] = "items/delete/$1";
+
 
---
+++ application/views/content.php
@@ -0,0 +1,16 @@
+<div class="row">
+<div class="col-lg-8">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Real Time Traffic
+                            </div>
+
+                                                
+			<div class="demo-container">
+			<div id="placeholderRT" class="demo-placeholder"></div>
+		</div>
+
+                        </div>
+
+</div>
+</div>
